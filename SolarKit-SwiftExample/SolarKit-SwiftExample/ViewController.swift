5ca45785fce45b8a042e757b9549a50e168ce037
wyhazq <wyhazq@gmail.com>
2018-01-03 10:09:22 +0800

    create project

---
+++ SolarKit-SwiftExample/SolarKit-SwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolarKit-SwiftExample
+//
+//  Created by wyh on 2018/1/3.
+//  Copyright © 2018年 SolarKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
