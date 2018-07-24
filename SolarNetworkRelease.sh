0413a238c1259ce59dcc8ba1e61225cefe42ef5f
wyhazq <wyhazq@foxmail.com>
2018-07-24 23:02:20 +0800

    Release.sh

---
+++ SolarNetworkRelease.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#pod trunk register email@email.com '****'  --verbose
+
+read -p "intput tag: " aTag
+
+echo "tag is ${aTag}"
+
+read -n 1 -p "Any key to continue"
+
+git tag '${aTag}'
+git push --tags
+pod trunk push SolarNetwork.podspec
+
+carthage build --no-skip-current
+carthage archive SolarNetwork
