f291f35c7c0218343afe51a626dec90e560ce1d5
wyhazq <wyhazq@foxmail.com>
2018-07-24 23:15:41 +0800

    fix Release.sh

--- SolarNetworkRelease.sh
+++ SolarNetworkRelease.sh
@@ -7,9 +7,9 @@ echo "tag is ${aTag}"
 
 read -n 1 -p "Any key to continue"
 
-git tag '${aTag}'
+git tag "${aTag}"
 git push --tags
 pod trunk push SolarNetwork.podspec
 
 carthage build --no-skip-current
-carthage archive SolarNetwork
+#carthage archive SolarNetwork
